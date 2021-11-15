c87a214530d674be5907db81e5247c1a8dc7f6ce
sysu19351146 <61243285+sysu19351146@users.noreply.github.com>
2021-11-15 23:11:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ class StepLossAccInfo(Callback):
 
 
 def save_step(args,steps_loss,steps_eval,steps_train):
-    np.save("/step.npy",steps_loss["step"])
-    np.save("/loss.npy",steps_loss["loss_value"])
-    np.save("/train_acc.npy",steps_train["acc"])
-    np.save("/val_acc.npy",steps_eval["acc"])
+    np.save("step.npy",steps_loss["step"])
+    np.save("loss.npy",steps_loss["loss_value"])
+    np.save("train_acc.npy",steps_train["acc"])
+    np.save("val_acc.npy",steps_eval["acc"])
 
 
 
